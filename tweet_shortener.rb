a996f03b35c21d3d91a33919314cbec9f9f49b79
Cody Frank <cody.frank30@gmail.com>
2019-04-14 01:18:20 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+def dictionary
+  shorter_words = {
+    "hello" => "hi",
+    "to, two, too" => "2",
+    "for, four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter (tweet)
+  tweet_array = tweet.split(" ")
+    tweet_array.collect do |word|
+
+    end
+end
